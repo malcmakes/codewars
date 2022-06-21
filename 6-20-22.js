ec8ce570dde65f5123ccc839f3efaed5ac27746c
Malcolm <malcolmharrellmedia@gmail.com>
2022-06-21 16:18:59 -0700

    Daily kata upload 6-21-22

--- 6-20-22.js
+++ 6-20-22.js
@@ -5,7 +5,7 @@
 // Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
 
 // For example, when the input is green, output should be yellow.
-
+ 
   function updateLight(current) {
     switch(current){
         case 'red':
