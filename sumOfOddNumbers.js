d30ddb21e5debfc06c90ee8f9561601a9fed1671
Malcolm <malcolmharrellmedia@gmail.com>
2022-07-09 14:38:59 -0700

    returns sum of the numbers in the nth row

---
+++ sumOfOddNumbers.js
@@ -0,0 +1,26 @@
+//7-9-22
+// 7 kyu
+// Sum of odd numbers
+
+// Given the triangle of consecutive odd numbers:
+
+// 1
+// 3     5
+// 7     9    11
+// 13    15    17    19
+// 21    23    25    27    29
+// ...
+// Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+// 1 -->  1
+// 2 --> 3 + 5 = 8
+
+function sumOfOddNumbers(n){
+    let startPoint = n * (n - 1) + 1
+    let sum = 0
+
+    for(let i = 0; i < n; i++){
+        sum += startPoint + (i * 2)
+    }
+    return sum
+}
