3e6f660c9ae7b76620130170834c085323ea2dc3
Malcolm <malcolmharrellmedia@gmail.com>
2022-07-19 13:49:24 -0700

    returns string multiplied n times

---
+++ stringRepeat.js
@@ -0,0 +1,18 @@
+// 7/19/22
+// 8 kyu
+// String repeat
+
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+// Examples (input -> output)
+// 6, "I"     -> "IIIIII"
+// 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+function repeatStr (n, s) {
+    let result = [];
+
+    for (let i =0; i < n; i++){
+           result.push(s);   
+    }
+    return result.join('');
+  }
